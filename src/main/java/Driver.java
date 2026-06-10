10d82954fe35ca24dae2ad3485f4ac67acde0f6a
Naufal Fahreza <naufalfahreza328@gmail.com>
2026-06-10 21:41:34 +0700

    update

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,19 +1,21 @@
 public class Driver {
 
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-	}
+    public static void main(String[] args) {
 
+        Counter counter = new Counter();
+
+        System.out.println("Current count:" + counter.getCount());
+
+        counter.increaseBy(5);
+
+        System.out.println("Current count:" + counter.getCount());
+
+        counter.decreaseBy(2);
+
+        System.out.println("Current count:" + counter.getCount());
+
+        counter.multiplyBy(6);
+
+        System.out.println("Current count:" + counter.getCount());
+    }
 }
